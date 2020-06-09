b09d28b35a7bcfaacde74a7c87afcc5cce3a1440
wangweiran <wangweiran@huaqin.com>
2020-06-09 16:12:22 +0800

    1. add right panel 2. add time, click count

--- src/HomeServlet.java
+++ src/HomeServlet.java
@@ -4,23 +4,32 @@ import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class HomeServlet extends HttpServlet {
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) {
-		ClickCountDAO dao = new ClickCountDAO();
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        int count = 0;
+        ClickCountDAO dao = new ClickCountDAO();
+        HttpSession session = request.getSession(false);
 
-		dao.clickPlus();
+        if (null == session) {
+            dao.clickPlus();
+            count = dao.getCount();
 
-		int count = dao.getCount();
-		
-		try {
-			response.getWriter().println("<h1 align=\"center\"> total click count is " + count + "</h1>");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+            session = request.getSession(true);
+            session.setAttribute("count", String.valueOf(count));
+        } else {
+            count = Integer.parseInt((String)session.getAttribute("count"));
+        }
 
-		return;
-	}
+        try {
+            response.getWriter().write(String.valueOf(count));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return;
+    }
 
 };
