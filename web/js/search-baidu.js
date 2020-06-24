f03e7ac6a0150ed230424d388ca1de598f74a1f6
wangweiran <wangweiran@huaqin.com>
2020-06-24 19:12:17 +0800

    1. Add jquery 2. Add baidu search to index.html

---
+++ web/js/search-baidu.js
@@ -0,0 +1,23 @@
+javascript:
+
+$(document).keypress(function(e) {
+
+    if (13 == e.keyCode || 13 == e.which || 13 == e.charCode) {
+        $("#search-box").children("input[type='button']").trigger("click");
+    }
+});
+
+$(document).ready(function() {
+    $("#search-box").children("input[type='text']").val("");
+    $("#search-box").children("input[type='button']").click(
+        function () {
+            $("#search-box").children("input[type='text']").focus();
+            var baiduHeader = "https://www.baidu.com/s?ie=utf-8&f=12&tn=baidu&wd="
+            var keyWord = $("#search-box").children("input[type='text']").val();
+
+            if (null != keyWord && "" != keyWord) {
+                window.open(baiduHeader+keyWord);
+            }
+        }
+    );
+});
