363a61c81df43f276303dc206e723ba6e4d8dfd2
wang.weiran <wang.wei.ran@outlook.com>
2021-03-24 12:20:09 +0000

    using 8081 for http

--- myserver.sh
+++ myserver.sh
@@ -7,5 +7,5 @@ sudo docker run \
     -v $PWD/logs:$TOMCAT/logs \
     -v $PWD/mysql-connector-java-5.1.6.jar:$TOMCAT/lib/mysql-connector-java-5.1.6.jar \
     -e CLASSPATH=$TOMCAT/bin/bootstrap.jar:$TOMCAT/bin/tomcat-juli.jar:$TOMCAT/lib/mysql-connector-java-5.1.6.jar \
-    -p 80:8080 -p 443:443 \
-    -itd tomcat:latest
+    -p 8081:8080 -p 443:443 \
+    -itd --name server tomcat:latest
