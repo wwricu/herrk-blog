6708870ddff6c285f04e1fd428a9cff24ae3c7c4
wangweiran <wang.wei.ran@outlook.com>
2021-03-30 05:52:51 +0000

    remove it

--- myserver.sh
+++ myserver.sh
@@ -2,10 +2,10 @@
 TOMCAT=/usr/local/tomcat
 
 sudo docker run \
-    -v $PWD/myserver:$TOMCAT/webapps/myserver \
-    -v $PWD/conf:$TOMCAT/conf \
-    -v $PWD/logs:$TOMCAT/logs \
-    -v $PWD/mysql-connector-java-5.1.6.jar:$TOMCAT/lib/mysql-connector-java-5.1.6.jar \
+    -v `pwd`/myserver:$TOMCAT/webapps/myserver \
+    -v `pwd`/conf:$TOMCAT/conf \
+    -v `pwd`/logs:$TOMCAT/logs \
+    -v `pwd`/mysql-connector-java-5.1.6.jar:$TOMCAT/lib/mysql-connector-java-5.1.6.jar \
     -e CLASSPATH=$TOMCAT/bin/bootstrap.jar:$TOMCAT/bin/tomcat-juli.jar:$TOMCAT/lib/mysql-connector-java-5.1.6.jar \
-    -p 80:8080 -p 443:443 -itd \
+    -p 80:8080 -p 443:443 -d \
     --name server --restart=always tomcat:latest
