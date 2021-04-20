90a36b8917d3f4f872eb5a773239df87d9ec90b7
wang.weiran <wang.wei.ran@outlook.com>
2021-04-20 22:14:23 +0800

    build index-re.html

--- myserver/web/js/search-baidu.js
+++ myserver/web/js/search-baidu.js
@@ -1,24 +1,24 @@
 javascript:
 
 $(document).keypress(function(e) {
-
     if (13 == e.keyCode || 13 == e.which || 13 == e.charCode) {
-        $("#search-box").children("input[type='button']").trigger("click");
+        if ($("#search-input").is(":focus")) {
+            $("#search-button").trigger("click");
+        } else {
+            $("#search-input").focus();
+        }
     }
 });
 
 $(document).ready(function() {
-    $("#search-box").children("input[type='text']").val("");
-    $("#search-box").children("input[type='button']").click(
-        function () {
-            $("#search-box").children("input[type='text']").focus();
-            var baiduHeader = "https://www.baidu.com/s?ie=utf-8&f=12&tn=baidu_local&wd="
-            var keyWord = $("#search-box").children("input[type='text']").val();
-
-            if (null != keyWord && "" != keyWord) {
-                window.open(baiduHeader+keyWord, "_self");
-                $("#search-box").children("input[type='text']").val("");
-            }
+    $("#search-input").val("");
+    $("#search-button").click(function () {
+        $("#search-input").focus();
+        let baiduHeader = "https://www.baidu.com/s?ie=utf-8&f=12&tn=baidulocal&wd="
+        let keyWord = $("#search-input").val();
+        if (null != keyWord && "" != keyWord) {
+            window.open(baiduHeader+keyWord, "_self");
+            $("#search-input").val("");
         }
-    );
+    });
 });
