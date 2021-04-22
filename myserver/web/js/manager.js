3bd2a462ea8fcd8b87307788199ee1560bfd7d54
wangweiran <wangweiran@huaqin.com>
2021-04-22 14:27:37 +0800

    store

--- myserver/web/js/manager.js
+++ myserver/web/js/manager.js
@@ -1,12 +1,13 @@
 function bindManagers() {
     $("#class-manager").click(function() {
-        /* classData = {"list": [{
-                    "classId":1,
-                    "className":"1",
-                    "fatherId": 1,
-                    "group":0
-            }]} */
         $(".append").remove();
+        let classAdder = $("<div id=\"add-class\" class=\"append\" align=center></div>")
+            .append($("<button id=add-class-add class=add-class-btn>Add a new class</button>"))
+            .append($("<input id=add-class-name class=add-class-btn type=\"text\" placeholder=\"input class name\" style=\"display: none;\">"))
+            .append($("<button id=add-class-submit class=add-class-btn style=\"display: none;\">confirm</button>"))
+            .append($("<button id=add-class-cancel class=add-class-btn style=\"display: none;\">cancel</button>"));
+        $("#right-frame").append(classAdder);
+        bindAddClass();
         $.ajax({
             type: "POST",
             async: true,
@@ -16,14 +17,21 @@ function bindManagers() {
             },
             dataType: "json",
             timeout: 1000,
+            /* result = {"list": [{
+                    "classId":1,
+                    "className":"1",
+                    "fatherId": 1,
+                    "group":0
+                }]} */
             success: function (result) {
                 if (result == "failure") {
                     return;
                 }
                 for (let i = 0; i < result.list.length; i++) {
                     let classCard = $("<div class=\"card append\"></div>")
-                    .append("<div class=container></div>")
-                        .append($("<h4 class = class-name align = \"left\"></h4>").text(result.list[i].className));
+                        .append($("<div class=container></div>")
+                                .append($("<h4 class = class-name align = \"left\"></h4>")
+                                    .text(result.list[i].className)));
                     $("#right-frame").append(classCard);
                 }
             }
@@ -31,18 +39,26 @@ function bindManagers() {
     });
 }
 
-function bindAddClass() {
-    $("#add-class-add").click(function() {
+function showAddBtn(show) {
+    if (show == false) {
         $("#add-class-add").hide();
         $("#add-class-name").show();
         $("#add-class-submit").show();
         $("#add-class-cancel").show();
-    });
-    $("#add-class-cancel").click(function() {
+    } else {
         $("#add-class-add").show();
         $("#add-class-name").hide();
         $("#add-class-submit").hide();
         $("#add-class-cancel").hide();
+    }
+}
+
+function bindAddClass() {
+    $("#add-class-add").click(function() {
+        showAddBtn(false);
+    });
+    $("#add-class-cancel").click(function() {
+        showAddBtn(true);
     });
     $("#add-class-submit").click(function() {
         let className = $("#add-class-name").val();
@@ -66,16 +82,12 @@ function bindAddClass() {
                 if (result == "failure") {
                     return;
                 }
-                $("#add-class-add").show();
-                $("#add-class-name").hide();
-                $("#add-class-submit").hide();
-                $("#add-class-cancel").hide();
+                showAddBtn(true);
             }
         });
     });
 }
 
 $(function() {
-    bindAddClass();
     bindManagers();
 });
