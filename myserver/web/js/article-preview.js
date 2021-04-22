5dd325aa04b95424dc087f876d453cd04045cebf
wang.weiran <wang.wei.ran@outlook.com>
2021-04-23 00:34:18 +0800

    1. remove re postfix 2. fix viewer.html 3. fixing editor.html

--- myserver/web/js/article-preview.js
+++ myserver/web/js/article-preview.js
@@ -1,106 +1,127 @@
+let articleLeft = true;
 let articleIndex = 0;
-let articleNum = 0;
+let classId = 0;
 
-function getArticleNum() {
-    $.ajax({
-        type: "POST",
-        async: true,
-        url: "articleviewer",
-        data: {"action": "getnum"},
-        dataType: "text",
-        timeout: 1000,
-        success: function (num) {
-            articleNum = parseInt(num);
-        }
-    });
-}
-
-function addArticle() {
+function addArticle(num) {
     "use strict";
-    let article = {
-        "article_id": 0,
-        "auther_id": 0,
-        "auther_name": "",
-        "title": "",
-        "summary": "",
-        "tags": "",
-        "bodyMD": "",
-        "create_time": "",
-        "last_modify_time": "",
-        "permission": 0
-    };
-
     $.ajax({
         type: "POST",
         async: false,
         url: "articleviewer",
+        /* receive = { list:[{
+            "article_id": 0,
+            "auther_id": 0,
+            "class_id": 0,
+            "class_name": "",
+            "title": "",
+            "summary": "",
+            "tags": "",
+            "bodyMD": "",
+            "create_time": "",
+            "last_modify_time": "",
+            "permission": 0
+        }];*/
         data: {
             "action": "preview",
+            "classId": classId,
             "index": articleIndex,
+            "num": num,
             "order": "last_modify_time"
         },
         dataType: "json",
         timeout: 1000,
         success: function (receive) {
-            if (!(receive.article_id > 0)) {
-                article.article_id = -1;
+            if (receive.list.length > 0) {
+                articleIndex += receive.list.length;
+                if (receive.list.length < num) {
+                    articleLeft = false;
+                }
+
+                for (let i = 0; i < receive.list.length; i++) {
+                    let link = '../viewer.html?id=' + receive.list[i].article_id;
+                    let articleCard = $("<div class=\"card article-append\"></div>")
+                        .append("<div class=container></div>")
+                        .append($("<a class=article-title href=\"" + link + "\"align=\"left\"></a>")
+                                    .text(unescape(receive.list[i].title)))
+                        .append($("<hr class='title-sum-hr'>"))
+                        .append($("<p class = article-summary align = \"left\"></p>")
+                                .text(unescape(receive.list[i].summary)));
+                    $("#left-frame").append(articleCard);
+                }
                 return;
             }
-            article.article_id = receive.article_id;
-            article.auther_id = receive.auther_id;
-            article.auther_name = receive.auther_name;
-            article.title = unescape(receive.title);
-            article.summary = unescape(receive.summary);
-            article.tags = unescape(receive.tags);
-            article.create_time = receive.create_time;
-            article.last_modify_time = receive.last_modify_time;
+            articleLeft = false;
         }
     });
-    if (article.article_id <= 0) {
-        return -1;
-    }
-    let link = '../viewer.html?id=' + article.article_id;
-    let title = "<a class = article-title href = \"" + link + "\"align = \"left\"></a>";
-    let articleBody = $("<div class = article ></div>")
-        .append($(title).text(article.title))
-        .append($("<div class = article-preview align = \"left\"></div>").text(article.summary))
-        .append($("<div class = article-auther align = \"right\"></div>")
-                .append($("<span class=crttime-prefix></span>").text("Created time: "))
-                .append($("<span class=created-time></span>").text(article.create_time))
-                .append($("<span class=auther-prefix></span>").text("Auther: "))
-                .append($("<span class=auther-name></span>").text(article.auther_name)));
-    $(".articleZone").append(articleBody);
-    articleIndex++;
+}
+
+function getClasses() {
+    $.ajax({
+        type: "POST",
+        async: true,
+        url: "classmanager",
+        data: {
+            "action": "allclasses"
+        },
+        dataType: "json",
+        timeout: 1000,
+        success: function(result) {
+            if (result == "failure") {
+                return;
+            }
+            for (let i = 0; i < result.list.length; i++) {
+                let localClassId = result.list[i].classId;
+                let button = "<button id=class-" + localClassId + " class='class-append item-title'></button>"
+                let classCard = $("<div class = \"card-item\"></div>")
+                        .append($(button).text(result.list[i].className))
+                        .append($("<span class = item-stat></span>").text(result.list[i].articleCount));
+                $("#class-container").append(classCard);
+
+                /*
+                   when the user click a specific class,
+                   its font would go bigger to hint the user which class he is viewing.
+                   when the user click anther class,
+                   all of the font go back to their origin size except the one he choose.
+                 */
+                $("#class-" + localClassId).on("click", function() {
+                    $(".article-append").remove();
+                    articleLeft = true;
+                    articleIndex = 0;
+
+                    $(".class-append").css("font-size","1.2rem");
+                    if (classId == localClassId) {
+                        classId = 0;
+                    } else {
+                        classId = localClassId;
+                        $("#class-" + localClassId).css("font-size","1.5rem");
+                    }
+
+                    renderPage();
+                });
+            }
+        }
+    });
 }
 
 function scrollLoad() {
     "use strict";
     $(window).scroll(function() {
         if ($(document).scrollTop() + $(window).height() >= $(document).height() - 1) {
-            // console.log($(document).scrollTop() + $(window).height() + $(document).height());
-            if (articleIndex < articleNum) {
-                addArticle();
+            if (articleLeft) {
+                addArticle(1);
             }
         }
     });
 }
 
 function renderPage() {
-    while (articleIndex < articleNum &&
-            $(window).height() == $(document).height()) {
-        addArticle();
-    }
+    addArticle(5);
 }
 
 $(document).ready(function () {
     "use strict";
-    for (let i = 0; i < 10; i++) {
-        if (addArticle() < 0) {
-            break;
-        }
-    }
-    getArticleNum();
+    getClasses();
     renderPage();
-    setInterval(renderPage,2000);
+    // setInterval(renderPage(0),2000);
     scrollLoad();
 });
